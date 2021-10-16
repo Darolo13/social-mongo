6db2c54bfe1c595c5b117a81a1d6bec607dcfe7e
Darolo13 <70618345+Darolo13@users.noreply.github.com>
2021-10-16 01:23:34 -0700

    GET a single user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,31 @@ const userController = {
     getAllUsers(req, res) {
         User.find()
         .populate({
-            path: 'Thought',
+            path: 'thoughts',
             select: '-__v'
         })
         .select('-__v')
         .then(dbUserData => res.json(dbUserData))
         .catch(err => {
             console.log(err);
-            res.status(500).json(err);
+            res.sendStatus(400);
+        });
+    },
+
+    // get a single user by its _id and populated thought and friend data
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+        .populate({
+            path: 'thoughts',
+            select: '-__v'
+        })
+        .select('-_v')
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(400);
         });
     }
-}
+};
 
 module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,18 @@
 const router = require('express').Router();
+
+const {
+    getAllUsers,
+    getUserById
+} = require('../../controllers/user-controller');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+
+module.exports = router;
